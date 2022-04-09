1f3b7dd0f1596a306051364fefdccf9730df7efe
manoj <manojk2829@gmail.com>
2022-04-09 18:09:42 +0530

    New Added Class Done.

---
+++ src/test/java/testPack/TestC.java
@@ -0,0 +1,17 @@
+package testPack;
+
+import org.testng.annotations.Test;
+
+public class TestC {
+	
+	@Test
+	public void test_c(){
+		System.out.println("Test C");
+	}
+	
+	@Test
+	public void test_c1(){
+		System.out.println("Test 1 CC");
+	}
+
+}
